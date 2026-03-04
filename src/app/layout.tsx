98b9aed6d567245616d02734d4c071d5202c0d1a
Homerunking27 <jaroslav.korcak@gmail.com>
2026-03-04 15:20:15 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,7 @@
 import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"], display: "swap" });
 
 export const metadata = {
   title: "Rezidence Pětidomí",
@@ -8,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="cs">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
